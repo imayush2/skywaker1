c0ee6b882f92641cb12191ae02ffb66e7df088c6
Ayush <imayush19052003@gmail.com>
2025-03-17 13:02:16 +0530

    server

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ const port = process.env.PORT;
 
 // Update CORS configuration
 const corsOptions = {
-<<<<<<< HEAD
   origin: "http://localhost:3000", // Change this to the correct frontend URL
-=======
-  origin:
-    "https://67c945ceb59fabc547398f49--tourmaline-fairy-89f1f6.netlify.app", // Change this to the correct frontend URL
->>>>>>> 7d9a521d454b94558a3a67b1f2178d4277c1de8d
   credentials: true, // Allow cookies to be sent
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
 };
@@ -36,17 +31,17 @@ app.get("/", (req, res) => {
   res.send("this is home page.");
 });
 
-// Example route to fetch data from MySQL
-app.get("/api/data", (req, res) => {
-  const query = "SELECT * FROM your_table_name"; // Replace with your MySQL table name
-  mysqlConnect.query(query, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: "Failed to retrieve data from MySQL" });
-    } else {
-      res.json(results); // Send MySQL data as JSON
-    }
-  });
-});
+// // Example route to fetch data from MySQL
+// app.get("/api/data", (req, res) => {
+//   const query = "SELECT * FROM your_table_name"; // Replace with your MySQL table name
+//   mysqlConnect.query(query, (err, results) => {
+//     if (err) {
+//       res.status(500).json({ error: "Failed to retrieve data from MySQL" });
+//     } else {
+//       res.json(results); // Send MySQL data as JSON
+//     }
+//   });
+// });
 
 app.use("/api/auth", authRoute);
 
